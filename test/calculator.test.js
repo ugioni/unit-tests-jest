ceae1b049968e17052835e276bab41b827077a4e
Leandro Ugioni <leandro_ugioni@hotmail.com>
2022-09-24 00:15:41 -0300

    Update calculator.test.js

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,5 +1,9 @@
-const { somar, diminuir, multiplicar, dividir } = require("../src/calculator");
+const { somar, diminuir } = require("../src/calculator");
 
 test("Somar dois valores válidos", async () => {
   expect(somar(5, 10)).toStrictEqual(15);
+});
+
+test("diminuir dois valores válidos", async () => {
+  expect(diminuir(20, 10)).toStrictEqual(10);
 });
