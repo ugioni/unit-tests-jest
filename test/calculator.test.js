7b43aacb0f89638af472a854a386e24fb62f1e3e
Leandro Ugioni <leandro_ugioni@hotmail.com>
2022-09-24 11:20:25 -0300

    [sonar] quality gate test

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -3,15 +3,3 @@ const { somar,diminuir,multiplicar,dividir } = require("../src/calculator");
 test("Somar dois valores válidos", async () => {
   expect(somar(5, 10)).toStrictEqual(15);
 });
-
-test("diminuir dois valores válidos", async () => {
-  expect(diminuir(20, 10)).toStrictEqual(10);
-});
-
-test("multiplicar dois valores válidos", async () => {
-  expect(multiplicar(5, 10)).toStrictEqual(50);
-});
-
-test("dividir dois valores válidos", async () => {
-  expect(dividir(10, 5)).toStrictEqual(2);
-});
--- test/cnh.test.js
+++ test/cnh.test.js
@@ -3,7 +3,3 @@ const { obterCnh } = require("../src/cnh");
 test("validando cnh idade 18 anos", async () => {
     expect(obterCnh(21)).toBeTruthy();
 });
-
-test("validando cnh idade 15 anos", async () => {
-    expect(obterCnh(15)).toBeFalsy();
-});
