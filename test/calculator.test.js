8bbfc431d734c80c291464b6d43b2a6e34b2f44e
Leandro Ugioni <leandro_ugioni@hotmail.com>
2022-09-24 00:23:10 -0300

    [wip] change tests

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,9 +1,5 @@
-const { somar, diminuir } = require("../src/calculator");
+const { somar } = require("../src/calculator");
 
 test("Somar dois valores válidos", async () => {
   expect(somar(5, 10)).toStrictEqual(15);
-});
-
-test("diminuir dois valores válidos", async () => {
-  expect(diminuir(20, 10)).toStrictEqual(10);
 });
