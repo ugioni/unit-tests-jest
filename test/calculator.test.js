09c7ac0e5b260e4c3306b044782bcdea373765d3
Leandro Ugioni <leandro_ugioni@hotmail.com>
2022-09-24 11:05:42 -0300

    [feat] add new tests

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,5 +1,17 @@
-const { somar } = require("../src/calculator");
+const { somar,diminuir,multiplicar,dividir } = require("../src/calculator");
 
 test("Somar dois valores válidos", async () => {
   expect(somar(5, 10)).toStrictEqual(15);
+});
+
+test("diminuir dois valores válidos", async () => {
+  expect(diminuir(20, 10)).toStrictEqual(10);
+});
+
+test("multiplicar dois valores válidos", async () => {
+  expect(multiplicar(5, 10)).toStrictEqual(50);
+});
+
+test("dividir dois valores válidos", async () => {
+  expect(dividir(10, 5)).toStrictEqual(2);
 });
--- test/cnh.test.js
+++ test/cnh.test.js
@@ -1,5 +1,9 @@
 const { obterCnh } = require("../src/cnh");
 
-test("test", async () => {
-    
+test("validando cnh idade 18 anos", async () => {
+    expect(obterCnh(21)).toBeTruthy();
+});
+
+test("validando cnh idade 15 anos", async () => {
+    expect(obterCnh(15)).toBeFalsy();
 });
