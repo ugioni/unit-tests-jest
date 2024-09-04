3d47673f67d3486e3b718a5543d64fe3641ace93
Leandro Ugioni <leandro_ugioni@hotmail.com>
2024-09-04 18:53:14 -0300

    trabalho 01 turma 01

--- test/banco.test.js
+++ test/banco.test.js
@@ -4,11 +4,30 @@ describe('Testes da classe Banco', () => {
     let conta;
 
     beforeEach(() => {
-        conta = new Banco('Conta Ugioni', 2);
+        contaZerada = new Banco('Conta Zerada');
+        contaSaldo = new Banco('Conta Com Saldo', 10);
     });
 
     test('Deve depositar dinheiro corretamente', () => {
-        conta.depositar(7.7);
-        expect(conta.obterSaldo()).toBe(9.7);
+        contaSaldo.depositar(7.7);
+        expect(contaSaldo.obterSaldo()).toBe(17.7);
     });
+
+    test('Deve sacar dinheiro corretamente', () => {
+        contaSaldo.sacar(5);
+        expect(contaSaldo.obterSaldo()).toBe(5);
+    });
+
+    test('Deve transferir dinheiro corretamente', () => {
+        contaSaldo.transferir(3, contaZerada);
+        expect(contaZerada.obterSaldo()).toBe(3);
+        expect(contaSaldo.obterSaldo()).toBe(7);
+    });
+
+    test('Deve validar saque sem saldo', () => {
+        expect(() => {
+            contaSaldo.sacar(15);
+        }).toThrow('Saldo insuficiente');
+    });
+
 });
