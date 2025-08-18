e35aa0095c5ec1a2ce3c5f798c373cbada11eff7
Eduardo Ribarski Scalco <eduardo.8723@alunosatc.edu.br>
2025-08-18 20:51:58 -0300

    subindo

--- test/anagram.test.js
+++ test/anagram.test.js
@@ -3,6 +3,7 @@ const { isAnagram } = require("../src/anagram");
 test("Validando anagrama válido", async () => {
   expect(isAnagram("satc", "ctas")).toBeTruthy();
 });
+ 
 
 test("Validando anagrama inválido", async () => {
   expect(isAnagram("satc", "testes")).toBeFalsy();
