fe9d97f42ca5885c6a347b017a49643e736ee113
Leandro Ugioni <leandro_ugioni@hotmail.com>
2026-03-04 19:17:58 -0300

    Remove old tests and update coverage

--- test/cnh.test.js
+++
@@ -1,9 +0,0 @@
-const { obterCnh } = require("../src/cnh");
-
-test("Validando CNH com idade 21 anos", async () => {
-  expect(obterCnh(21)).toBeTruthy();
-});
-
-test("Validando CNH com idade 17 anos", async () => {
-  expect(obterCnh(17)).toBeFalsy();
-});
